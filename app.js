b6951f635a5587e1e6441d8074b7424165786ed9
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-10-15 22:53:39 +0300

    Integrated ejs as our view engine and applied changes to our .html files to reflect that.

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const path = require("path")
+const ejs = require("ejs");
 
 const app = express();
 
 const PORT = 5000;
 
+app.set("view engine", "ejs")
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo description',
-  };
 
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index")
 });
+
+app.get("/about", (req,res) => {
+  res.render("about")
+})
+
+app.get("/add_post", (req,res) => {
+  res.render("add_post")
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
