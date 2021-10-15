071d128831d557aa541d7d608384bf3780435b31
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-10-15 22:18:28 +0300

    Static files and entry to middleware

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const path = require("path")
+
+const app = express();
 
 const PORT = 5000;
 
-const app = express();
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo description',
+  };
+
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
