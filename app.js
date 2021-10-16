8f9f4092889f42222c5fccc3b4eacb7327cf06c2
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-10-16 17:52:12 +0300

    Updated add photo form. Added route for adding photos and some middleware for using form data.

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require('express');
-const path = require("path")
-const ejs = require("ejs");
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
 const PORT = 5000;
 
-app.set("view engine", "ejs")
-app.use(express.static("public"))
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTES
 app.get('/', (req, res) => {
-
-  res.render("index")
+  res.render('index');
 });
 
-app.get("/about", (req,res) => {
-  res.render("about")
-})
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-app.get("/add_post", (req,res) => {
-  res.render("add_post")
-})
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect("index")
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
